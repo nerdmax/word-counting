a966cce216b380c827ac539526b75801f1dd31b6
Max <mrliucan@foxmail.com>
2019-02-06 22:06:56 +1100

    re(wallaby): Add wallaby's config file

---
+++ wallaby.js
@@ -0,0 +1,27 @@
+module.exports = function(w) {
+  return {
+    files: ['src/**/*.ts', 'test/fixtures/entry.js'],
+
+    tests: ['test/**/*.test.ts'],
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    testFramework: 'jest',
+
+    preprocessors: {
+      '**/*.js?(x)': file =>
+        require('babel-core').transform(file.content, {
+          sourceMap: true,
+          plugins: ['transform-es2015-modules-commonjs'],
+          presets: ['babel-preset-jest']
+        })
+    },
+
+    hints: {
+      ignoreCoverage: 'istanbul ignore'
+    }
+  }
+}
