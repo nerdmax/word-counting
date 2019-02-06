3aaeaa758b88ebfaf878712002825dd51e47cefd
Max <mrliucan@foxmail.com>
2019-02-06 22:12:32 +1100

    test: add wordsCounter's initial test

---
+++ test/word-counting.test.ts
@@ -0,0 +1,25 @@
+import wordsCounter from '../src/word-counting'
+
+describe('word-counting', () => {
+  describe('with empty text', () => {
+    test('counts words', () => {
+      const mockedText = ''
+      expect(wordsCounter(mockedText).wordsCount).toEqual(0)
+    })
+  })
+
+  describe('with plain text', () => {
+    test('counts words', () => {
+      const mockedText = 'These are some mocked words.'
+      expect(wordsCounter(mockedText).wordsCount).toEqual(5)
+    })
+  })
+
+  describe('with html text', () => {
+    test('counts words', () => {
+      const mockedText = '<p>These are some mocked words with html format</p>'
+      expect(wordsCounter(mockedText).wordsCount).toEqual(10)
+      expect(wordsCounter(mockedText, { isHtml: true }).wordsCount).toEqual(8)
+    })
+  })
+})
