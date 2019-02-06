05f7bbb4d4a1978275e98f9fa38170b7c49cb1dd
Max <mrliucan@foxmail.com>
2019-02-07 10:28:39 +1100

    feat: add docs to lib

--- src/word-counting.ts
+++ src/word-counting.ts
@@ -4,15 +4,38 @@ Sentry.init({ dsn: 'https://d4dbb1184d474e20a856e0ce3ae1cbda@sentry.io/1387790' 
 const regex = require('word-regex')
 const h2p = require('html2plaintext')
 
-type Config = {
+/**
+ * The config settings for the wordsCounter function
+ * @interface Config
+ */
+interface Config {
+  /**
+   * Set to true if you want to process html formated text and the counter
+   * won't count tags.
+   * @memberof Config
+   */
   isHtml?: boolean
   isUnique?: boolean
 }
 
-type Result = {
+/**
+ * The result that the wordsCounter function return.
+ * @interface Result
+ */
+interface Result {
+  /**
+   * The amount of the words in the passed text
+   * @memberof Result
+   */
   wordsCount: number
 }
 
+/**
+ * The function that can count the words occurrence in text
+ * @param text The string that you want to count words
+ * @param config The config settings for the wordsCounter function
+ * @returns an object that has the words occurrence information
+ */
 const wordsCounter = (text: string, config?: Config): Result => {
   const result: Result = {
     wordsCount: 0
