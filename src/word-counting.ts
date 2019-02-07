2a7edd64a601d7b7f708eaa3e9e2a6a0ab20a0d2
Max <mrliucan@foxmail.com>
2019-02-08 10:33:31 +1100

    fix: remove sentry

--- src/word-counting.ts
+++ src/word-counting.ts
@@ -1,6 +1,3 @@
-import * as Sentry from '@sentry/browser'
-Sentry.init({ dsn: 'https://d4dbb1184d474e20a856e0ce3ae1cbda@sentry.io/1387790' })
-
 const regex = require('word-regex')
 const h2p = require('html2plaintext')
 
@@ -41,19 +38,13 @@ const wordsCounter = (text: string, config?: Config): Result => {
     wordsCount: 0
   }
 
-  try {
-    const plainText = config && config.isHtml ? h2p(text) : text
+  const plainText = config && config.isHtml ? h2p(text) : text
 
-    if (plainText.length > 0) {
-      const match = plainText.match(regex())
-      result.wordsCount = match && match.length
-    }
-  } catch (err) {
-    /* istanbul ignore next */
-    Sentry.captureException(err)
-    /* istanbul ignore next */
-    throw err
+  if (plainText.length > 0) {
+    const match = plainText.match(regex())
+    result.wordsCount = match && match.length
   }
+
   return result
 }
 
